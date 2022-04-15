8054d2ac0919eb286d0e7ea4e0078ee45ca45fba
Oleg Zasedatelev <ol.zasedatelew@yandex.ru>
2022-04-15 21:16:17 +0300

    change added

--- lesson_3/DZ_3.js
+++ lesson_3/DZ_3.js
@@ -1,30 +1,56 @@
-function Good (id, name, description, sizes, price,available) {
+//Good - класс для хранения данных о товаре со свойствами
+class Good {
+    constructor (id,name, description, sizes,price,available) {
     this.id = id;
     this.name = name;
     this.description = description;
-    this.sizes = sizes;
+    this.sizes = [sizes];
     this.price = price;
     this.available = 'available';
+    }
 
-    Good.prototype.constructor = function(){
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.sizes = sizes;
-        this.price = price;
-        this.available = 'available';  
-    };
-
-    Good.prototype.setAvailable = function(){
+    setAvailable (){
         this.available = 'not available';
-    };
-
-function Goodlist(){
-    #goods       
-    filter      
-    sortPrice  
-    sortDir 
-}
+    }
 
 }
 
+//GoodsList - класс для хранения каталога товаров со свойствами
+class Goodlist {
+    constructor (products=[]){
+        this.goods = products.forEach(element => {
+            return [element];
+        });
+        this.filter = '';
+        this.sortDir = products.filter(element.price=True=>{
+            return element;
+        });
+        this.sortPrice = sortPrice;
+    }
+
+    newProduct(id,name,description,sizes,price,available){
+        let product = new Good(id,name,description,sizes,price,available);
+        this.products.push(product);
+        return product;
+    }
+
+    getlist(){}
+    add(){}
+    remove(id){}
+}
+
+//BasketGood - класс дочерний от Good,
+//для хранения данных о товаре в корзине с дополнительным свойством
+class BasketGood extends Good{
+    constructor(id){
+    this.amount = id;
+    }
+}
+class Basket{
+    constructor(goods){
+        this.goods = '';
+    }
+
+    get totalAmount(){}
+    get totalSum(){}
+}
